212301e3eba0d2f529650edfd2e14fc554fa7584
Ben <bwochinski@gmail.com>
2015-01-30 11:47:21 +0000

    Added GUI updates on load

--- monkeys.js
+++ monkeys.js
@@ -9,9 +9,6 @@ var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P",
 var alphaDouble = ["AA","AB","AC","AD","AE","AF","AG","AH","AI","AJ","AK","AL","AM","AN","AO","AP","AQ","AR","AS","AT","AU","AV","AW","AX","AY","AZ","BA","BB","BC","BD","BE","BF","BG","BH","BI","BJ","BK","BL","BM","BN","BO","BP","BQ","BR","BS","BT","BU","BV","BW","BX","BY","BZ","CA","CB","CC","CD","CE","CF","CG","CH","CI","CJ","CK","CL","CM","CN","CO","CP","CQ","CR","CS","CT","CU","CV","CW","CX","CY","CZ","DA","DB","DC","DD","DE","DF","DG","DH","DI","DJ","DK","DL","DM","DN","DO","DP","DQ","DR","DS","DT","DU","DV","DW","DX","DY","DZ","EA","EB","EC","ED","EE","EF","EG","EH","EI","EJ","EK","EL","EM","EN","EO","EP","EQ","ER","ES","ET","EU","EV","EW","EX","EY","EZ","FA","FB","FC","FD","FE","FF","FG","FH","FI","FJ","FK","FL","FM","FN","FO","FP","FQ","FR","FS","FT","FU","FV","FW","FX","FY","FZ","GA","GB","GC","GD","GE","GF","GG","GH","GI","GJ","GK","GL","GM","GN","GO","GP","GQ","GR","GS","GT","GU","GV","GW","GX","GY","GZ","HA","HB","HC","HD","HE","HF","HG","HH","HI","HJ","HK","HL","HM","HN","HO","HP","HQ","HR","HS","HT","HU","HV","HW","HX","HY","HZ","IA","IB","IC","ID","IE","IF","IG","IH","II","IJ","IK","IL","IM","IN","IO","IP","IQ","IR","IS","IT","IU","IV","IW","IX","IY","IZ","JA","JB","JC","JD","JE","JF","JG","JH","JI","JJ","JK","JL","JM","JN","JO","JP","JQ","JR","JS","JT","JU","JV","JW","JX","JY","JZ","KA","KB","KC","KD","KE","KF","KG","KH","KI","KJ","KK","KL","KM","KN","KO","KP","KQ","KR","KS","KT","KU","KV","KW","KX","KY","KZ","LA","LB","LC","LD","LE","LF","LG","LH","LI","LJ","LK","LL","LM","LN","LO","LP","LQ","LR","LS","LT","LU","LV","LW","LX","LY","LZ","MA","MB","MC","MD","ME","MF","MG","MH","MI","MJ","MK","ML","MM","MN","MO","MP","MQ","MR","MS","MT","MU","MV","MW","MX","MY","MZ","NA","NB","NC","ND","NE","NF","NG","NH","NI","NJ","NK","NL","NM","NN","NO","NP","NQ","NR","NS","NT","NU","NV","NW","NX","NY","NZ","OA","OB","OC","OD","OE","OF","OG","OH","OI","OJ","OK","OL","OM","ON","OO","OP","OQ","OR","OS","OT","OU","OV","OW","OX","OY","OZ","PA","PB","PC","PD","PE","PF","PG","PH","PI","PJ","PK","PL","PM","PN","PO","PP","PQ","PR","PS","PT","PU","PV","PW","PX","PY","PZ","QA","QB","QC","QD","QE","QF","QG","QH","QI","QJ","QK","QL","QM","QN","QO","QP","QQ","QR","QS","QT","QU","QV","QW","QX","QY","QZ","RA","RB","RC","RD","RE","RF","RG","RH","RI","RJ","RK","RL","RM","RN","RO","RP","RQ","RR","RS","RT","RU","RV","RW","RX","RY","RZ","SA","SB","SC","SD","SE","SF","SG","SH","SI","SJ","SK","SL","SM","SN","SO","SP","SQ","SR","SS","ST","SU","SV","SW","SX","SY","SZ","TA","TB","TC","TD","TE","TF","TG","TH","TI","TJ","TK","TL","TM","TN","TO","TP","TQ","TR","TS","TT","TU","TV","TW","TX","TY","TZ","UA","UB","UC","UD","UE","UF","UG","UH","UI","UJ","UK","UL","UM","UN","UO","UP","UQ","UR","US","UT","UU","UV","UW","UX","UY","UZ","VA","VB","VC","VD","VE","VF","VG","VH","VI","VJ","VK","VL","VM","VN","VO","VP","VQ","VR","VS","VT","VU","VV","VW","VX","VY","VZ","WA","WB","WC","WD","WE","WF","WG","WH","WI","WJ","WK","WL","WM","WN","WO","WP","WQ","WR","WS","WT","WU","WV","WW","WX","WY","WZ","XA","XB","XC","XD","XE","XF","XG","XH","XI","XJ","XK","XL","XM","XN","XO","XP","XQ","XR","XS","XT","XU","XV","XW","XX","XY","XZ","YA","YB","YC","YD","YE","YF","YG","YH","YI","YJ","YK","YL","YM","YN","YO","YP","YQ","YR","YS","YT","YU","YV","YW","YX","YY","YZ","ZA","ZB","ZC","ZD","ZE","ZF","ZG","ZH","ZI","ZJ","ZK","ZL","ZM","ZN","ZO","ZP","ZQ","ZR","ZS","ZT","ZU","ZV","ZW","ZX","ZY","ZZ",];
 
 var wordCount = 0;
-var numLetters = 0;
-
-var bBills = 10;
 
 var primateName = ["iMonkey", "iGorilla", "King Kong", "Monkey King"];
 var primateIncr = [10, 100, 200, 1000];
@@ -33,7 +30,9 @@ var Player = {
       primateNum: [0,0,0,0],
       primateCost: [10,100,200,1000],
       letterRate: 1,   // single, double, triple per tick
-      bBillsMult: 1
+      bBillsMult: 1,
+      bBills: 10,
+      numLetters: 0
     };
     
 setTimeout('document.getElementById("firstLoad").innerHTML = \'<h3 style="margin-top: 0px;">Load A Text<h3>\'', 1000);
@@ -46,7 +45,7 @@ function driver() {
 // Buy Things
 
 function hire(type, num) {
-	if (bBills >= Player.primateCost[type]) {
+	if (Player.bBills >= Player.primateCost[type]) {
 		Player.primateNum[type] += 1;
 		changeBBills(-Player.primateCost[type]);
 
@@ -66,7 +65,7 @@ function hire(type, num) {
 }
 
 function buyJaneGoodall() {
-	if (bBills < JGoodallCost) {
+	if (Player.bBills < JGoodallCost) {
 		setTimeout('document.getElementById("buyJGoodallCost").innerHTML = "Banana Bills: " + JGoodallCost;', 1000);
 		document.getElementById("buyJGoodallCost").innerHTML = "<h4>" + "Banana Bills: " + JGoodallCost + "</h4>";
 	}
@@ -82,7 +81,7 @@ function buyJaneGoodall() {
 }
 
 function buyDoubleLetters() {
-	if (bBills < doubleLettersCost) {
+	if (Player.bBills < doubleLettersCost) {
 		setTimeout('document.getElementById("doubleLettersCost").innerHTML = "Banana Bills: " + doubleLettersCost;', 1000);
 		document.getElementById("doubleLettersCost").innerHTML = "<h4>" + "Banana Bills: " + doubleLettersCost + "</h4>";
 	}
@@ -109,8 +108,8 @@ function calcTime() {
 function changeBBills(change) {
 	if (change>0 && Player.bBillsMult == 2) 
 		change += change;
-	bBills += change;
-	document.getElementById("bBills").innerHTML = "<b>Banana Bills: </b> " + bBills;
+	Player.bBills += change;
+	document.getElementById("bBills").innerHTML = "<b>Banana Bills: </b> " + Player.bBills;
 }
 
 function manualMoney() {
@@ -129,17 +128,17 @@ function randomText() {
 		if (output.length > 39) {
 			document.getElementById("out").innerHTML = output;
 			output = "";
-			document.getElementById("numLetters").innerHTML = "<b>Letters:</b> " + numLetters + "</p>";
+			document.getElementById("numLetters").innerHTML = "<b>Letters:</b> " + Player.numLetters + "</p>";
 		}
 	}
 	else {
 		document.getElementById("out").innerHTML = output;
-		document.getElementById("numLetters").innerHTML = "<b>Letters:</b> " + numLetters + "</p>";
+		document.getElementById("numLetters").innerHTML = "<b>Letters:</b> " + Player.numLetters + "</p>";
 	}
 }
 
 function genLetters() {
-	numLetters += Player.letterRate;
+	Player.numLetters += Player.letterRate;
 	letterSpeed += Player.letterRate;
 
 	if (Player.letterRate == 1) {
@@ -374,7 +373,7 @@ $.get( "ospd.txt", function( txt ) {
 	driver();
 });
 
-//Save Game
+//Save Gamegit s
 
 function SaveGame() {
   window.localStorage.setItem("iMonkey-Player", JSON.stringify(Player)); //set localstorage for player info
@@ -384,6 +383,25 @@ function SaveGame() {
 function LoadGame() {
   $.extend(true, Player, JSON.parse(window.localStorage.getItem("iMonkey-Player"))); //load player
   console.log("Game Loaded");
+  
+  //Update GUI
+  document.getElementById("bBills").innerHTML = "<b>Banana Bills: </b> " + Player.bBills;
+  document.getElementById("numLetters").innerHTML = "<b>Letters:</b> " + Player.numLetters + "</p>";
+  
+  for (var el in Player.primateNum) {
+  	document.getElementById(primateName[el]+"Cost").innerHTML = "Banana Bills: " + Player.primateCost[el];
+		document.getElementById("num"+primateName[el]).innerHTML = "<b>"+primateName[el]+"s:</b> " + Player.primateNum[el];
+  }
+  
+  if (Player.bBillsMult == 2) {
+		document.getElementById("JGoodallBTN").disabled = true;
+		document.getElementById("buyJGoodallCost").innerHTML = "<b>Earned Banana <br>Bills x2</b>";
+  }
+  
+  if (Player.letterRate == 2) {
+		document.getElementById("doubleLettersBTN").disabled = true;
+		document.getElementById("doubleLettersCost").innerHTML = "<b>Each monkey types<br>two letters at a time</b>";
+  }
 }
 
 function ResetGame() {
